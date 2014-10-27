76b9272e994ef037782fcb8159f0a651dfcccd5e
Denis Dreano <denis.dreano@gmail.com>
2014-10-27 14:21:55 +0300

    cleaned parameters list of simu_npz

--- scripts/npz/simu_npz.m
+++ scripts/npz/simu_npz.m
@@ -4,9 +4,9 @@ close all
 
 %% Simu parameters
 
-%------------- True Model parameters -------------------%
+% Unknown Model Parameters ------------------------------------------------
 
-% Unknown parameters
+% NPZ parameters
 MU    = 2.0;
 K     = 0.5;
 G     = 1.0;
@@ -15,17 +15,22 @@ EP    = 0.02;
 EZ    = 0.01;
 EP_   = 0.1;
 EZ_   = 0.1;
+
 THETA = [MU; K; G; GAMMA; EP; EZ; EP_; EZ_];
 theta = transform_state(THETA);
 
-% Known parameters
-alpha_phi = .7;
+% Known Model Parameters --------------------------------------------------
 
-% True Observations parameters --------------------------------------------
+% Phi Auto-Regressive Coefficient
+alpha = .7;
 
-C = 2; % known
+% Known Observations Parameters -------------------------------------------
 
-% True State initialization ------------%
+% Conversion rate phytoplankton -> Chl
+C = 2;
+c = log(C);
+
+% True State Initialization -----------------------------------------------
 
 phi0 = 0.1;
 N0 = .1;
@@ -33,35 +38,35 @@ P0 = .1;
 Z0 = .01;
 x0 = [log(N0); log(P0); log(Z0); phi0];
 
-%-------------- Model function ----------------------------%
+% Model function ----------------------------------------------------------
 
 npz = @(x, theta, alpha) npz_predict2(x, theta, alpha);
-Nx = size(x0, 1);
+Nx = size(x0, 1); % nb of state
 
-%-------------- Observation function ----------------------%
+% Observation function ----------------------------------------------------
 
 H = zeros(1, Nx); H(2) = 1;
-h   = @(x, c) H * x + C;
-No = 1;   % size of observation vector
+h   = @(x, c) H * x + c;
+No = 1;   % nb of obs
 
-%-------------- True Model Noise -----------------------%
+% True Model Noise --------------------------------------------------------
 
 sigma_phi = 0.05; % phi model noise
 sigma_x = 0.032 * ones(3,1); % CI of ± 5% around model forecast
 sqQ = diag([sigma_x; sigma_phi]);
 
-%-------------- True Observation noise ------------------%
+% True Observation noise --------------------------------------------------
 
 sigmao = 0.06;           % obs noise deviation, CI of ± 10% around true value
 sqR    = sigmao*eye(No); % obs noise covariance
 
-%------------- Simulation parameters ---------------------%
+% Simulation parameters ---------------------------------------------------
 
 T  = 30; % nb of time steps
 
 %% Generate Observations
 
-[obs, truth] = gen_obs(npz, h, x0, sqQ, sqR, T, theta, alpha_phi, C);
+[obs, truth] = gen_obs(npz, h, x0, sqQ, sqR, T, theta, alpha, C);
 
 %% Plot observations
 
